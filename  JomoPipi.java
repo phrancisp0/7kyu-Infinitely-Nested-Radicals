dae6767579f847522651fe01603ee5de87ab1484
Francis Patawah <phrancisp0@gmail.com>
2023-09-21 10:51:43 -0400

    infinitely nested radicals 7kyu

---
+++  JomoPipi.java
@@ -0,0 +1,62 @@
+public class JomoPipi {
+  public static double evaluateFunction(int x) {
+        return Math.sqrt(x+.25)+.5;
+  }
+}
+/*
+Hello,
+
+I am Jomo Pipi
+
+and today we will be evaluating an expression like this:
+
+(there are an infinite number of radicals)
+
+�
++
+�
++
+�
++
+�
++
+⋯
+x+ 
+x+ 
+x+ 
+x+⋯
+​
+ 
+​
+ 
+​
+ 
+​
+ 
+
+for a given value x
+
+Simple!
+
+arguments passed in will be 1 or greater
+
+(Check out my other kata!)
+
+Matrix Diagonal Sort OMG
+
+String -> N iterations -> String
+
+String -> X iterations -> String
+
+ANTISTRING
+
+Array - squareUp b!
+
+Matrix - squareUp b!
+
+Infinitely Nested Radical Expressions
+
+pipi Numbers!
+
+
+*/
